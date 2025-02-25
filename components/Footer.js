f68063096ce5d62a49707b43f725dee6be1b4702
Abdulbaqui07 <abdulbaqui.2000@gmail.com>
2025-02-25 11:45:17 +0530

    Added Footer

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer-main">
+            <p className='info-para'>Developed and maintained by <a href='https://github.com/DEREK-DEV-AFK' target='_blank'>Sufiyan</a></p>
+        </div>
+    )
+}
+
+export default Footer
