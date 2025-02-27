71de3ef35e1cef1706b2e571b2ed27ec2f0f72fc
Abdulbaqui07 <abdulbaqui.2000@gmail.com>
2025-02-27 16:26:09 +0530

    Updated Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className="footer-main">
-            <p className='info-para'>Developed and maintained by <a href='https://github.com/DEREK-DEV-AFK' target='_blank'>Sufiyan</a></p>
+            <p className='info-para'>Developed and maintained by <a href='https://github.com/Abdulbaqui07' target='_blank'>Abdulbaqui</a></p>
         </div>
     )
 }
